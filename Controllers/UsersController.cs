c4a97ee2f6b656c7cd089d0f47a568528940678b
Habid Enrique Manzur Restrepo <habibmanzur@outlook.com>
2023-07-08 16:52:54 -0500

    feat: added more route actions

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,21 +3,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace Discoteque.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class UsersController : ControllerBase
+[Route("users")]
+public class UserController : ControllerBase
 {
-    private readonly ILogger<UsersController> _logger;
+    private readonly ILogger<UserController> _logger;
 
-    public UsersController(ILogger<UsersController> logger)
+    public UserController(ILogger<UserController> logger)
     {
         _logger = logger;
     }
 
-    [HttpGet()]
-    public string get()
+    [HttpGet]
+    public string getAll()
     {
         // var result = new { status = "healthy" };
         // return Ok(result);
         return "Building";
+    }
+
+
+    [HttpGet]
+    [Route("{id}")]
+    public string GetById(string id)
+    {
+        // var result = new { status = "healthy" };
+        // return Ok(result);
+        return $"Building {id}";
     }
 }
